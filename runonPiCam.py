681b87026845648341b0512aaa2f1d1ec2a37f23
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-10 18:33:10 +0100

    second version files

--- runonPiCam.py
+++ runonPiCam.py
@@ -15,48 +15,49 @@ size, center_x = 0, 0 #initial size & center x point of the hand = 0
 
 #initialize variables for calculating FPS
 fps = 0
-frame_counter = 0
+image_counter = 0
 start_time = time.time()
 
 # DECLARE PICAMERA
 camera = PiCamera ()
 camera.resolution = (840, 480)
-camera.vflip = true
+camera.vflip = True
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size= (840, 480))
 time.sleep(0.5) #allow picamera to warm up
 
 # CAPTURE CONTINUOUS STREAM
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array #store each incoming image as a numpy array
+    
+   # cv2.imshow("Frame", image)
 
-    while (True):
+   # while (True):
 
         # Read frame by frame
-        ret, frame = image.read()
-
-        if not ret:
-            break
+        # cv2.imshow ("Frame", image)
 
         # Laterally flip the frame
-        frame = cv2.flip(frame, 1)
+    image = cv2.flip(image, 1)
 
         # Calculate the Average FPS
-        frame_counter += 1
-        fps = (frame_counter / (time.time() - start_time))
+    image_counter += 1
+    fps = (image_counter / (time.time() - start_time))
 
         # Create a clean copy of the frame
-        copy = frame.copy()
+    copy = image.copy()
 
         # Downsize the frame.
-        new_width = int(frame.shape[1] / scale_factor)
-        new_height = int(frame.shape[0] / scale_factor)
-        resized_frame = cv2.resize(copy, (new_width, new_height))
+    new_width = int(image.shape[1] / scale_factor)
+    new_height = int(image.shape[0] / scale_factor)
+    resized_image = cv2.resize(copy, (new_width, new_height))
 
         # Detect with detector
-        detections = detector(resized_frame)
+    detections = detector(resized_image)
 
         # Loop for each detection.
-        for detection in (detections):
+    for detection in (detections):
             # Since we downscaled the image we will need to resacle the coordinates according to the original image.
             x1 = int(detection.left() * scale_factor)
             y1 = int(detection.top() * scale_factor)
@@ -64,8 +65,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             y2 = int(detection.bottom() * scale_factor)
 
             # Draw the bounding box
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, 'Hand Detected', (x1, y2 + 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 2)
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(image, 'Hand Detected', (x1, y2 + 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 2)
 
             # Calculate size of the hand.
             size = int((x2 - x1) * (y2 - y1))
@@ -74,14 +75,14 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             center_x = x2 - x1 // 2
 
         # Display FPS and size of hand
-        cv2.putText(frame, 'FPS: {:.2f}'.format(fps), (20, 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 2)
+    cv2.putText(image, 'FPS: {:.2f}'.format(fps), (20, 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 2)
 
         # This information is useful for when you'll be building hand gesture applications
-        cv2.putText(frame, 'Center: {}'.format(center_x), (540, 20), cv2.FONT_HERSHEY_COMPLEX, 0.5, (233, 100, 25))
-        cv2.putText(frame, 'size: {}'.format(size), (540, 40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (233, 100, 25))
+    cv2.putText(image, 'Center: {}'.format(center_x), (540, 20), cv2.FONT_HERSHEY_COMPLEX, 0.5, (233, 100, 25))
+    cv2.putText(image, 'size: {}'.format(size), (540, 40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (233, 100, 25))
 
         # Display the image
-        cv2.imshow('frame', frame)
+    cv2.imshow('frame', image)
 
     #cv2.imshow("Frame", image) #display each new incoming image
     key = cv2.waitKey(1) & 0xFF
